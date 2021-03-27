452c2faa0c7810897d2c134552af2d0cffa79ba9
Janeiii <76029679+Janeiii@users.noreply.github.com>
2021-03-26 23:30:56 -0700

    sigin created

--- frontend/shop/src/App.js
+++ frontend/shop/src/App.js
@@ -3,6 +3,7 @@ import {Route, withRouter} from "react-router-dom";
 
 import Welcome from "./pages/welcome/welcome";
 import Register from "./pages/register/register";
+import Login from "./pages/login/login";
 
 
 function App(props) {
@@ -10,6 +11,7 @@ function App(props) {
     <div>
       {props.location.pathname === "/"? null : <Header/>}
         <Route exact path="/" component={Welcome} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
     </div>
   );
